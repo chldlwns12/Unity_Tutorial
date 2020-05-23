c3711234395e61f51a0615dcd8b7425a5680d4f9
chldlwns12 <chldlwns12@gmail.com>
2020-05-24 00:51:21 +0900

    2020.05.24 유니티 튜토리얼 2~7강 2. 트랜스폼 3. 리지드 바디 4. 컬라이더 5. 메쉬 렌더러 6. 카메라 7. 라이트

---
+++ Unity_Tutorial/Assets/Scripts/Test.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Test : MonoBehaviour
+{
+    Vector3 rotation;
+    [SerializeField]
+    private GameObject go_camera;
+
+    void Start()
+    {
+        //rotation = this.transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            //이동
+            //this.transform.position = this.transform.position + new Vector3(0, 0, 1) * Time.deltaTime; // 60분의 1
+            //this.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime);
+
+            //앵글
+            //this.transform.eulerAngles = transform.eulerAngles + new Vector3(90, 0, 0) * Time.deltaTime;
+            //rotation = rotation + new Vector3(90, 0, 0) * Time.deltaTime;
+            //this.transform.eulerAngles = rotation;
+            //Debug.Log(transform.eulerAngles);
+
+            //this.transform.Rotate(new Vector3(90, 0, 0) * Time.deltaTime);
+
+            //rotation = rotation + new Vector3(90, 0, 0) * Time.deltaTime;   //한축을 고정했을때 다른축의 회전이 고장하는 상황이 생기는데 짐볼락(?) 이라고 한다.
+            //this.transform.rotation = Quaternion.Euler(rotation);           //Quaterniom 을 쓰는이유는 짐볼락이 생기지 않는다.
+
+            //크기
+            //this.transform.localScale = this.transform.localScale + new Vector3(2, 2, 2) * Time.deltaTime;
+
+            //대표적인 속성 예
+            //moveSpeed * this.transform.forward * Time.deltaTime  //정규화 벡터(방향만을 알려주는 녀석) new Vector3(0,0,1)
+            //moveSpeed * this.transform.up * Time.deltaTime  //정규화 벡터(방향만을 알려주는 녀석) new Vector3(0,1,0)
+            //moveSpeed * this.transform.right * Time.deltaTime  //정규화 벡터(방향만을 알려주는 녀석) new Vector3(1,0,0)
+
+            //원하는 오브젝트 바라보기
+            //this.transform.LookAt(go_camera.transform.position);
+        }
+        //오브젝트를 중심으로 공전하기
+        //transform.RotateAround(go_camera.transform.position, Vector3.up, 100 * Time.deltaTime);
+    }
+}
---
+++ Unity_Tutorial/Assets/Scripts/Test5.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test5 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject go_Target;
+    
+    [SerializeField]
+    private float speed;
+    
+    private Vector3 difVealue;
+
+    //private Camera theCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        difVealue = transform.position - go_Target.transform.position;
+        //difVealue = new Vector3(Mathf.Abs(difVealue.x), Mathf.Abs(difVealue.y), Mathf.Abs(difVealue.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, go_Target.transform.position + difVealue, speed);
+    }
+}
