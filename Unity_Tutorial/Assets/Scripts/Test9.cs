e1dde2cba4374be17b1bdcf80e1a11ab7a32173c
chldlwns12 <chldlwns12@gmail.com>
2020-05-24 03:16:31 +0900

    2020.05.24 유니티 튜토리얼 9. 애니메이터 10.파티클 시스템

---
+++ Unity_Tutorial/Assets/Scripts/Test9.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test9 : MonoBehaviour
+{
+
+    ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //시작
+        //ps.Play();
+        
+        //정지
+        //ps.Stop();
+
+        //일시정지
+        //ps.Pause();
+
+        //버스트 같이 한번에 몇개의 파티클을 재샐할지
+        //ps.Emit(100);
+    }
+}
