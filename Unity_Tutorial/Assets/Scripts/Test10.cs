7bb7e63d392f3aa540667ba4b452cfecca3ff06a
chldlwns12 <chldlwns12@gmail.com>
2020-05-24 13:09:30 +0900

    2020.05.24 유니티 튜토리얼 11. UI의 기본

---
+++ Unity_Tutorial/Assets/Scripts/Test10.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test10 : MonoBehaviour
+{
+    [SerializeField]
+    private Text txt_name;
+    [SerializeField]
+    private Image img_name;
+    [SerializeField]
+    private Sprite sprite;
+
+    private bool isCoolTime = false;
+
+    private float currentTime = 5f;
+    private float delayTime = 5f;
+
+    public void Change()
+    {
+        txt_name.text = "변경됨";
+        isCoolTime = true;
+        //img_name.fillAmount = 0.5f;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //img_name.color = Color.red;
+        Color color = img_name.color;
+        color.a = 0f;
+        img_name.color = color;
+
+        if (isCoolTime)
+        {
+            currentTime -= Time.deltaTime;
+            img_name.fillAmount = currentTime / delayTime;
+
+            if(currentTime <= 0)
+            {
+                isCoolTime = false;
+                currentTime = delayTime;
+                img_name.fillAmount = currentTime;
+            }
+        }
+    }
+}
